8c2c8fa7171eeb84c1a019e657b3a195e922c7a0
Jacob Fish <jacobfish@quickenloans.com>
2020-02-21 09:30:05 -0500

    Secondary Commit

--- PrimeNumbersList/PrimeNumbers.cs
+++ PrimeNumbersList/PrimeNumbers.cs
@@ -16,12 +16,6 @@ namespace PrimeNumbersList
                     PrimeList.Add(i);
                 if (i == 3)
                     PrimeList.Add(i);
-                //if (i == 5)
-                //    PrimeList.Add(i);
-                //if (i == 7)
-                //    PrimeList.Add(i);
-                //if (i == 11)
-                //    PrimeList.Add(i);
                 if (i > 3)
                 {
                     bool isPrime = true;
@@ -30,26 +24,16 @@ namespace PrimeNumbersList
                     {
                         if (i % j == 0)
                         {
-                            //break;
                             isPrime = false;
                             break;
                         }
-                        //else
-                        //{
-                        //    PrimeList.Add(i);
-                        //    break;
-
-                        //}
                     }
                     if (isPrime == true)
                         PrimeList.Add(i);
-
-
                 }
             }
             return PrimeList[a - 1];
         }
-
     }
 }
 
